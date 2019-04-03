c371f7ff52b34947efccec81b7aa7be1990f76f5
Joaquin Peñalver <joaquin.penalver@gmail.com>
2019-04-04 00:50:50 +0200

    testing externalized coniguration

---
+++ src/main/java/com/joak/spring/amqp/config/PropertiesEvaluator.java
@@ -0,0 +1,19 @@
+package com.joak.spring.amqp.config;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PropertiesEvaluator {
+
+
+
+	@PostConstruct
+	public void evaluate() {
+
+
+
+	}
+
+}
